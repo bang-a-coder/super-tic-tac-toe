34c8fd3f0248d1f3fa72341b5baced6ef3661436
bang-a-coder <nikitas4717@gmail.com>
2024-05-01 00:40:08 +0300

    recursion bitch

--- app/src/lib/GameBoard.svelte
+++ app/src/lib/GameBoard.svelte
@@ -1,35 +1,80 @@
 <script>
-    export let size = 3
-    export let totem = 'X'
-    let board = Array(size).fill().map(() => Array(size).fill(null));
-    //[ [ null, null, null ],
-    //  [ null, null, null ],
-    //  [ null, null, null ] ]
-    let activeCells = 0
+    export let size
+    export let totem
+    let activeCells
+    let board = []
 
     function cellClickHander(row, cell) {
+        if (board[row][cell] !== null) {return}
         
         board[row][cell] = totem
 
-        totem = totem === 'X' ? 'O' : 'X';
         activeCells++
-        console.log(winCheck())
+        console.log(winCheck(row, cell))
         console.log(board)
+        totem = totem === 'X' ? 'O' : 'X';
     }
 
-    function winCheck() {
-        let rowWin = board.some((row) => {
+    function winCheck(ri, ci) {
+        //Check rowwise
+        let rowTest = board.some((row)=>{
             return row.every(cell => cell === row[0] && cell !== null)
         })
 
-        let diaWin = board.every((row, i) => {
-            return (row[i] === board[0][0]) || (row[i] === board[0][2])
-        })
+        //Check colwise (colonoscopy hahah)
+        let colTest = (ref) => {
+            if (ref > 2) return false
 
-        if (rowWin) return rowWin
-        if (diaWin) return diaWin
-        return false
+            let test = board.every((row, i)=>{
+                return (row[ref] === board[0][ref]) && (row[ref] !== null)
+            })
+
+            if (test == true) return true
+            
+            return colTest(ref+1)
+        }
+        
+        if (rowTest == true) return 'Row wins'
+        if (colTest(0) == true) return 'Col wins'
+        
+        
+        
+        
+        
+        
+        
+        // if (activeCells <= 2) return "No winner yet"
+
+        // let rowWin = board.some((row) => {
+        //     return row.every(cell => cell === row[0] && cell !== null)
+        // })
+
+        // let diaWin = board.every((row, i) => {
+        //     return (row[i] === board[0][0]) || (row[i] === board[0][2])
+        // })
+
+        // let verWin = board.every((row, i) => {
+        //     return board.every()
+        // })
+
+        // if (rowWin == true) return `${totem} wins in a row!`
+        // if (diaWin == true) return `${totem} wins in diagonal`
+        // return false
     }
+
+    function _initBoard() {
+        size = 3
+        totem = 'X'
+        activeCells = 0
+
+        board =  Array(size).fill().map(() => Array(size).fill(null));
+
+        //[ [ null, null, null ],
+        //  [ null, null, null ],
+        //  [ null, null, null ] ]
+    }
+
+    _initBoard()
 </script>   
 
 <style>
@@ -39,6 +84,7 @@
       gap: 5px;
       justify-items: center;
       max-width: 400px;
+      max-height: 400px;
       margin: 100px auto;
       border-collapse: collapse;
     }
@@ -53,6 +99,7 @@
       cursor: pointer;
       border: 1px solid black;
       aspect-ratio: 1 / 1;
+      user-select: none;
     }
 </style>
 
@@ -67,3 +114,5 @@
         {/each}
     {/each}
 </div>
+
+<button on:click={()=>_initBoard()}>Reset</button>
